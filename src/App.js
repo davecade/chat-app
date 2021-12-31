522b48500bafec8f3a0bdb23a3dff72e34ae75c3
Dave <dave.cadelina@outlook.com>
2021-12-31 17:21:27 +1100

    base config done for chat app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { ChatEngine } from 'react-chat-engine'
+import { ChatEngine } from "react-chat-engine";
 
-import './App.css'
+import "./App.css";
 
 const App = () => {
-    return (
-        <ChatEngine
-            
-        />
-    )
-}
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='admin'
+			userSecret='123123'
+			projectID='39e2f4e3-8ba9-41c6-ac72-a232c22ab811'
+		/>
+	);
+};
+
+export default App;
