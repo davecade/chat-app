92a7e37cae96e840fe164cdd6bd8f9b40fc02f56
Dave <dave.cadelina@outlook.com>
2022-01-02 22:21:40 +1100

    implementing chat feed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -11,7 +11,29 @@ const ChatFeed = (props) => {
 
     const chat = chats && chats[activeChat];
 
-    console.log(chat, userName, messages)
+    const renderMessages = () => {
+        const keys = Object.keys(messages)
+
+        return keys.map((key, index) => {
+            const message = messages[key]
+            const lastMessageKey = index === 0 ? null : keys[index - 1]
+            const isMyMessage = userName === message.sender.username
+
+            return (
+                <div key={`msg_${index}`} style={{width: '100%'}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage />
+                            : <TheirMessage />
+                        }
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    renderMessages()
 
     return (
         <div>
